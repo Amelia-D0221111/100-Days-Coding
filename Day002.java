ce1b00dfd9bf6304ef9adb925a8f80a440388b48
amelia-d0221111 <amelia132018@gmail.com>
2023-10-11 15:58:09 +0800

    Hari ke-2

---
+++ Day002.java
@@ -0,0 +1,10 @@
+public class Day002 {
+    public static void main(String[] args) {
+        String nama, alamat;
+        nama = "Amelia";
+        alamat = "Polewali";
+        int umur = 20;
+        System.out.println("nama saya " + nama + ", saya berasal dari " + alamat +
+        " dan saya berumur " + umur);
+    }
+}
