38ee2933e55a46d0bff99ecd68681cacff268a2f
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2023-10-14 23:11:41 +0800

    Add files via upload

---
+++ Day003.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		double angka1 = 5;
+		double angka2 = 4;
+		double hasil = angka1 + angka2 ;
+		System.out.println("hasil dari " + angka1 + " + " + angka2 + " = " + hasil);
+	}
+}
