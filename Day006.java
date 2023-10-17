f84a3ddf03500ecc7ead508a2fe0805fee19a415
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2023-10-17 23:09:44 +0800

    Belajar Aritmatika

---
+++ Day006.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int angka1 , angka2 ;
+		angka1 = 10;
+		angka2 = 2 ;
+		System.out.println("hasil perkalian dari " + angka1 + " x " + angka2 + " = " + angka1*angka2);
+		System.out.println("hasil penjumlahan dari " + angka1 + " + " + angka2 + " = " + angka1+angka2);
+System.out.println("hasil pengurangan dari " + angka1 + " - " + angka2 + " = " + (angka1-angka2));
+System.out.println("hasil pembagian dari " + angka1 + " / " + angka2 + " = " + angka1/angka2);
+		}
+}
