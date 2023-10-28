881d63f2e6fd586c505b51d22601312366f87deb
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2023-10-28 20:03:14 +0800

    Delete Day015.java

--- Day015.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		int nilai1 = 1;
-		double nilai2 = 9.0;
-		
-		double nilai1Konver = nilai1;
-		int nilai2Konver = (int)nilai2;
-	}
-}
