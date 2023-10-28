da374bf5f9020ba92b9c82a919164fa396a58877
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2023-10-28 19:52:27 +0800

    Konversi

---
+++ Day015.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		int nilai1 = 1;
+		double nilai2 = 9.0;
+		
+		double nilai1Konver = nilai1;
+		int nilai2Konver = (int)nilai2;
+	}
+}
