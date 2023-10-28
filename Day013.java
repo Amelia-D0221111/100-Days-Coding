174c17e322af21ef925a18872bcc5f4bf2b1a945
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2023-10-28 20:06:55 +0800

    Nilai Max dan Min integer

---
+++ Day013.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		int max = Integer.MAX_VALUE;
+		int min = Integer.MIN_VALUE;
+		
+		System.out.println("Nilai Max Integer: "+max);
+			System.out.println("Nilai Min Integer: "+min);
+	}
+}
