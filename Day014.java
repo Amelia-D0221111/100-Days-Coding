1c68b25af859609c1b4672f59931faa9baedd7e7
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2023-10-28 20:03:00 +0800

    Delete Day013.java

--- Day014.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		int max = Integer.MAX_VALUE;
-		int min = Integer.MIN_VALUE;
-		
-		System.out.println("Nilai Max Integer: "+max);
-			System.out.println("Nilai Min Integer: "+min);
-	}
-}
