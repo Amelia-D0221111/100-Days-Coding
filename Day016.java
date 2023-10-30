6270d5235ab6d9a9e2cfc292b0081a9815aa6e77
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2023-10-30 14:49:01 +0800

    ArrayList

---
+++ Day016.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+ 
+        String nama = "Amelia";
+        System.out.println("Nama: " + nama);
+        
+        Integer umur = 20;
+        System.out.println("Umur: " + umur);
+
+        ArrayList<String> daftarNama = new ArrayList<>();
+        daftarNama.add("Amel");
+        daftarNama.add("Mel");
+        daftarNama.add("Melia");
+        System.out.println("Daftar Nama: " + daftarNama);
+    }
+}
