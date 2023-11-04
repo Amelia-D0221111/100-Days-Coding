93522f5abe86c478e1d7a5a516b90de97be2eb8b
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2023-11-04 20:06:18 +0800

    Konversi non primitive ke primitive

---
+++ Day021.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Konversi String ke int
+        String strInt = "123";
+        int intValue = Integer.parseInt(strInt);
+        System.out.println("String to int: " + intValue);
+
+        // Konversi Double ke double
+        Double doubleObj = 3.14159;
+        double doubleValue = doubleObj; 
+        System.out.println("Double to double: " + doubleValue);
+
+        // Konversi Character ke char
+        Character charObj = 'A';
+        char charValue = charObj; 
+        System.out.println("Character to char: " + charValue);
+
+        // Konversi Boolean ke boolean
+        Boolean boolObj = true;
+        boolean boolValue = boolObj; 
+        System.out.println("Boolean to boolean: " + boolValue);
+    }
+}
