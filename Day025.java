15509129538a7dd0ebe4fb6232c9400f7cc26b08
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2023-11-09 00:12:52 +0800

    Mengkonversi array menjadi List

---
+++ Day025.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Day025 {
+    public static void main(String[] args) {
+        String[] array = {"Mobil", "Motor", "Pesawat", "Kapal"};
+        List<String> list = Arrays.asList(array);
+        System.out.println("List: " + list);
+    }
+}
