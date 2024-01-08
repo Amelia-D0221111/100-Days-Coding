5c1c43aff071ab1323cd9280a6705b83755561dd
Amelia-D0221111 <100204034+Amelia-D0221111@users.noreply.github.com>
2024-01-08 11:40:18 +0800

    Mengganti Kata

---
+++ Day053.py
@@ -0,0 +1,10 @@
+kalimat = """
+Amelia Informatika H
+Amelia ISC
+Amelia Unsulbar
+"""
+
+kata_pengganti = input("Masukkan kata ganti : ")
+hasil_manipulasi = kalimat.replace("Amelia", kata_pengganti)
+
+print(hasil_manipulasi)
